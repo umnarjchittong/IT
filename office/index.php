f2615055ff8b5c29c125efa2d7c214733e67cbe4
umnarj chittong <umnarjchittong@@gmail.com>
2023-02-23 15:23:33 +0700

    office practise

---
+++ office/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>MS OFFICE by UMNARJ</title>
+
+    <!-- Facebook Open Graph -->
+    <meta property="og:url" content="https://faed.mju.ac.th/it/office" />
+    <meta property="og:type" content="web" />
+    <meta property="og:title" content="MS OFFICE by @UMNARJ" />
+    <meta property="og:description" content="เว็บไซต์สำหรับเอกสารประกอบการอบรม MS OFFICE ม.แม่โจ้" />
+    <meta property="og:image" content="../assets/img/profile_pic.jpg" />
+
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="../assets/img/profile_pic.png" />
+
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="../css/styles.css" rel="stylesheet" />
+
+</head>
+
+<body id="page-top" style="font-size: 0.8rem; font-family: prompt">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top " id="">
+        <div class="container">
+            <a class="navbar-brand text-uppercase text-white fw-bolder" href="#page-top">UMNARJ@MJU</a>
+            <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto fs-6">
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded text-white" href="#page-top">หน้าแรก</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded text-white" href="#it_system">ลิงก์เอกสาร</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded text-white" href="#contact">ติดต่อ</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead bg-primary text-white text-center">
+        <div class="container d-flex align-items-center flex-column">
+            <!-- Masthead Avatar Image-->
+            <img class="masthead-avatar mb-5" src="../assets/img/profile_pic.png" alt="..." />
+            <!-- Masthead Heading-->
+            <h3 class="text-uppercase mb-0">(ต้อม) อำนาจ ชิดทอง</h3>
+            <!-- Icon Divider-->
+            <div class="divider-custom divider-light">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <!-- Masthead Subheading-->
+            <p class="masthead-subheading font-weight-light mb-0">วิทยากร</p>
+        </div>
+    </header>
+    <!-- It System Section-->
+    <section class="page-section it_system" id="it_system">
+        <div class="container">
+            <!-- Portfolio Section Heading-->
+            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">ลิงก์เอกสาร</h2>
+            <!-- Icon Divider-->
+            <div class="divider-custom">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <!-- Portfolio Grid Items-->
+            <div class="row justify-content-center my-5 py-3">
+                <div class="text-uppercase col-md-5 col col-12">
+                    <p class="mb-3 fs-4">สไลด์ประกอบการสอน</p>
+                    <ol class="text-capitalize fs-5">
+                        <!-- <li><a href="Formal Note-2565.pdf" target="_blank" class=" text-decoration-none">MS WORD หนังสือราชการ (PDF)</a></li> -->
+                        <li><a href="https://www.canva.com/design/DAFI4xaHTys/-FettspC7KKHb0VrET-Xnw/view?utm_content=DAFI4xaHTys&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="_blank" class=" text-decoration-none">MS EXCEL (slide)</a></li>
+                        <li><a href="excel-umnarj.pdf" target="_blank" class=" text-decoration-none">MS EXCEL (PDF)</a></li>
+                        <li><a href="https://www.canva.com/design/DAFJffG3AGg/10EWi0wOL5XMU154aVOT2w/view?utm_content=DAFJffG3AGg&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="_blank" class=" text-decoration-none">MS WORD (slide)</a></li>
+                        <li><a href="word-umnarj.pdf" target="_blank" class=" text-decoration-none">MS WORD (PDF)</a></li>
+                    </ol>
+                </div>
+                <div class="text-uppercase col-md-5 col-12">
+                    <p class="fs-4 mb-3">เอกสารอื่นๆ</p>
+                    <ol class="text-capitalize fs-5">
+                        <!-- <li><a href="word-เฉลย.pdf" target="_blank" class=" text-decoration-none">เฉลย Workshop MS WORD</a></li> -->
+                        <li><a href="workshop_สหกิจศึกษา.xlsx" target="_blank" class=" text-decoration-none">ไฟล์ Workshop MS EXCEL</a></li>
+                        <li><a href="word-workshop.docx" target="_blank" class=" text-decoration-none">ไฟล์ Workshop MS WORD</a></li>
+                    </ol>
+                </div>
+            </div>
+            <div>บทที่ 1 บทนำ
+ความเป็นมา
+วัตถุประสงค์
+ผลที่คาดว่าจะได้รับ
+ขอบเขต
+บทที่ 2 เอกสารงานวิจัยที่เกี่ยวข้อง
+ผลงานที่เกี่ยวข้อง
+ทบทวนวรรณกรรม
+ 
+บทที่ 3 การดำเนินการวิจัย
+แผนการดำเนินการ
+วิธีการวิจัย
+บทที่ 4 ผลการวิจัย
+ผลการดำเนินการ
+บทที่ 5 บทสรุป
+อภิปรายผล
+ข้อแนะนำ
+ภาคผนวก
+ดัชนี
+</div>
+
+        </div>
+    </section>
+    
+    <!-- Contact Section-->
+    <section class="page-section bg-primary text-white" id="contact">
+        <div class="container">
+            <!-- Contact Section Heading-->
+            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">ติดต่อ</h2>
+            <!-- Icon Divider-->
+            <div class="divider-custom">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <!-- Contact Section Form-->
+            <div class="row justify-content-center">
+                <div class="col-lg-8 col-xl-7 text-center mt-5">
+                    <a class="btn btn-lg btn-info me-2 me-lg-5" href="https://m.me/umnarj" target="_blank" style="min-width: 180px;">
+                        <i class="bi bi-facebook me-2"></i>
+                        Facebook
+                    </a>
+                    <a class="btn btn-lg btn-success" href="https://line.me/ti/p/T7suE5YNVU" target="_blank" style="min-width: 180px;">
+                        <i class="bi bi-line me-2"></i>
+                        Line
+                    </a>
+                </div>
+                <div class="col-lg-8 col-xl-7 text-center mt-5">
+                    <a class="btn btn-lg btn-warning" href="tel:+66866581883" style="min-width: 180px;">
+                        <i class="bi bi-phone-fill me-2"></i>
+                        โทรด่วนตอนนี้
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Copyright Section-->
+    <div class="copyright py-4 text-center text-white">
+        <div class="container fs-6"><small>Copyright &copy; UMNARJ CHITTONG, 2022</small></div>
+    </div>
+    
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <!-- <script src="js/scripts.js"></script> -->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script> -->
+</body>
+
+</html>
